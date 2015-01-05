6b3e052f1ccc34ff47e80e7eb0eb0b976200aa7a
Mecin <marusm4@gmail.com>
2015-01-05 21:52:47 +0100

    Finally added working live stream view using Gstreamer SDK and Android NDK.

--- app/src/main/java/pl/dmcs/mecin/raspic/RaspiControlFragmentSocket.java
+++ app/src/main/java/pl/dmcs/mecin/raspic/RaspiControlFragmentSocket.java
@@ -1,23 +1,31 @@
 package pl.dmcs.mecin.raspic;
 
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.gstreamer.GStreamer;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -31,14 +39,32 @@ import java.net.UnknownHostException;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RaspiControlFragmentSocket extends Fragment {
+public class RaspiControlFragmentSocket extends Fragment implements SurfaceHolder.Callback {
 
-    private long DELAY = 300;
+    //ndk sample test
+    //public native String  stringFromJNI();
+
+    private native void nativeInit();     // Initialize native code, build pipeline, etc
+    private native void nativeFinalize(); // Destroy pipeline and shutdown native code
+    private native void nativePlay();     // Set pipeline to PLAYING
+    private native void nativePause();    // Set pipeline to PAUSED
+    private static native boolean nativeClassInit(); // Initialize native class: cache Method IDs for callbacks
+    private native void nativeSurfaceInit(Object surface);
+    private native void nativeSurfaceFinalize();
+    private long native_custom_data;      // Native code will use this to keep private data
+
+    private boolean is_playing_desired;   // Whether the user asked to go to PLAYING
+
+    // 5 fps
+    private long DELAY = 200;
+
     private String CAMERA_IMAGE = "camera.jpg";
     private String CAMERA_IMAGE_URL = "";
     private String RECEIVED_IP = "";
     private int PORT = 5000;
     private boolean STREAMING_FLAG = false;
+    private boolean CHANGE_IMG_MUTEX = false;
+    private long startTime;
 
     private GetCameraImage getCameraImage;
     private Socket socket;
@@ -80,15 +106,59 @@ public class RaspiControlFragmentSocket extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_raspi_control_fragment_socket, container, false);
 
-        // Camera image
-        ImageView cameraImageView = (ImageView) view.findViewById(R.id.camera_image);
-        cameraImageView.setImageResource(R.drawable.iconpi);
+        // Initialize GStreamer and warn if it fails
+        try {
+            GStreamer.init(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            Toast.makeText(getActivity().getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+            //finish();
+            //return;
+        }
 
-        // Set flag to start stream
-        STREAMING_FLAG = true;
+        // play gstreamer stream button
+        ImageButton play = (ImageButton) view.findViewById(R.id.button_play);
+        play.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                is_playing_desired = true;
+                nativePlay();
 
-        // Get camera image from Raspberry PI
-        new MJPEGStream(cameraImageView).execute(CAMERA_IMAGE_URL);
+                if(outToServer != null) {
+                    try {
+                        // write 9 - forward
+                        Log.d("gstreamer start","sending gstreamer start signal");
+                        outToServer.writeByte(9);
+                        Log.d("gstreamer","done");
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        // pause gstreamer stream button
+        ImageButton pause = (ImageButton) view.findViewById(R.id.button_stop);
+        pause.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                is_playing_desired = false;
+                nativePause();
+
+                if(outToServer != null) {
+                    try {
+                        // write 10 - forward
+                        Log.d("gstreamer stop","sending gstreamer stop signal");
+                        outToServer.writeByte(10);
+                        Log.d("gstreamer","done");
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        // gstreamer surfaceview
+        SurfaceView sv = (SurfaceView) view.findViewById(R.id.surface_video);
+        SurfaceHolder sh = sv.getHolder();
+        sh.addCallback(this);
 
         // forward button
         Button forwardButton = (Button) view.findViewById(R.id.forward_button);
@@ -176,12 +246,6 @@ public class RaspiControlFragmentSocket extends Fragment {
             public void onClick(View v) {
                 if(outToServer != null) {
                     try {
-
-                        //Log.d("ORIENTATION", "to horizontal");
-                        // Change layout orientation
-                        //LinearLayout currentLinearLayout = (LinearLayout) v.findViewById(R.id.socket_layout);
-                        //currentLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
-
                         // write 5 - stop
                         Log.d("stop","sending stop signal");
                         outToServer.writeByte(5);
@@ -193,7 +257,100 @@ public class RaspiControlFragmentSocket extends Fragment {
             }
         });
 
+        // low gear button
+        Button lowGearButton = (Button) view.findViewById(R.id.low_gear_button);
+        lowGearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(outToServer != null) {
+                    try {
+                        // write 1 - stop
+                        Log.d("low gear","sending low gear signal");
+                        outToServer.writeByte(1);
+                        Log.d("low gear", "done");
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        // mid gear button
+        Button midGearButton = (Button) view.findViewById(R.id.mid_gear_button);
+        midGearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(outToServer != null) {
+                    try {
+                        // write 7 - stop
+                        Log.d("mid gear","sending mid gear signal");
+                        outToServer.writeByte(7);
+                        Log.d("mid gear", "done");
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        // top gear button
+        Button topGearButton = (Button) view.findViewById(R.id.max_gear_button);
+        topGearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(outToServer != null) {
+                    try {
+                        // write 3 - stop
+                        Log.d("top gear","sending top gear signal");
+                        outToServer.writeByte(3);
+                        Log.d("top gear", "done");
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+        nativeInit();
+
         return view;
+    }
+
+    public void onDestroy() {
+        nativeFinalize();
+        super.onDestroy();
+    }
+
+    // Called from native code. This sets the content of the TextView from the UI thread.
+    private void setMessage(final String message) {
+        final TextView tv = (TextView) getActivity().findViewById(R.id.textview_message);
+        getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                tv.setText(message);
+            }
+        });
+    }
+
+    // Called from native code. Native code calls this once it has created its pipeline and
+    // the main loop is running, so it is ready to accept commands.
+    private void onGStreamerInitialized () {
+        Log.i ("GStreamer", "Gst initialized. Restoring state, playing:" + is_playing_desired);
+        // Restore previous playing state
+        if (is_playing_desired) {
+            nativePlay();
+        } else {
+            nativePause();
+        }
+
+        // Re-enable buttons, now that GStreamer is initialized
+        //final Activity activity = this;
+        getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                //activity.findViewById(R.id.button_play).setEnabled(true);
+                //activity.findViewById(R.id.button_stop).setEnabled(true);
+            }
+        });
     }
 
     @Override
@@ -289,8 +446,6 @@ public class RaspiControlFragmentSocket extends Fragment {
                 e.printStackTrace();
             }
 
-            //bmImage.setImageBitmap(mIcon11);
-
             return mIcon11;
         }
 
@@ -300,44 +455,91 @@ public class RaspiControlFragmentSocket extends Fragment {
         }
     }
 
-    private class MJPEGStream extends AsyncTask<String, Void, ImageView> {
+    private class MJPEGStream extends AsyncTask<String, Void, Bitmap> {
         ImageView bmImage;
 
         public MJPEGStream(ImageView bmImage) {
             this.bmImage = bmImage;
         }
 
-        protected ImageView doInBackground(String... urls) {
-            Log.d("doInBg MJPEG", "enter");
+        protected Bitmap doInBackground(String... urls) {
+            startTime = System.currentTimeMillis();
+            Log.d("doInBg MJPEG", "enter startTime: " + startTime);
+
             if(getCameraImage != null) {
-                getCameraImage.cancel(true);
+                //getCameraImage.cancel(true);
             }
             //while(STREAMING_FLAG) {
                 Log.d("while(true)", "before GetCamImg");
-                // Get camera image from Raspberry PI
-                getCameraImage = (GetCameraImage)new GetCameraImage(bmImage).execute(urls[0]);
 
-                // Sleep for debug 5s
+                // Get camera image from Raspberry PI
+                //getCameraImage = (GetCameraImage)new GetCameraImage(bmImage).execute(urls[0]);
+
+                Bitmap piCameraBitmap = null;
                 try {
-                    Thread.sleep(DELAY);
-                } catch (InterruptedException e) {
+                    InputStream in = new java.net.URL(urls[0]).openStream();
+                    piCameraBitmap = BitmapFactory.decodeStream(in);
+                } catch (Exception e) {
+                    Log.e("Error", e.getMessage());
                     e.printStackTrace();
                 }
+
+                // Sleep for 40ms = max 25 fps if possible
+                //try {
+                //    Thread.sleep(DELAY);
+                //} catch (InterruptedException e) {
+                //    e.printStackTrace();
+                //}
             //}
 
-            return bmImage;
+            return piCameraBitmap;
         }
 
-        protected void onPostExecute(ImageView bmImage) {
+        protected void onPostExecute(Bitmap piCameraBitmap) {
             Log.d("onPostExecute", "MJPEGStream");
+
+            //while(CHANGE_IMG_MUTEX) {
+
+            //}
+
+            //CHANGE_IMG_MUTEX = true;
+
+            while((System.currentTimeMillis() - startTime) < DELAY) {
+                // get stable frames
+            }
+
+            this.bmImage.setImageBitmap(piCameraBitmap);
+
+            Log.d("STOP", "STOP TIME: " + (System.currentTimeMillis() - startTime) + " ms.");
+            //CHANGE_IMG_MUTEX = false;
 
             // Get next image
             if(STREAMING_FLAG) {
                 //getCameraImage.cancel(true);
-                new MJPEGStream(bmImage).execute(CAMERA_IMAGE_URL);
+                new MJPEGStream(this.bmImage).execute(CAMERA_IMAGE_URL);
             }
-
-            //bmImage.setImageBitmap(result);
         }
+    }
+
+    static {
+        System.loadLibrary("gstreamer_android");
+        System.loadLibrary("tutorial-3");
+        nativeClassInit();
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+        Log.d("GStreamer", "Surface changed to format " + format + " width "
+                + width + " height " + height);
+        nativeSurfaceInit (holder.getSurface());
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d("GStreamer", "Surface created: " + holder.getSurface());
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.d("GStreamer", "Surface destroyedsss");
+        nativeSurfaceFinalize ();
     }
 }
